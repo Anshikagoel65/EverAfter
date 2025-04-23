dbca16f29c018221afdde66356dd8449edf8f410
Anshika <anshikagoel1701@gmail.com>
2025-04-23 23:29:55 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -2,22 +2,43 @@ const express = require("express");
 const app = express();
 const { adminAuth, userAuth } = require("./middleware/auth");
 
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.post("/user/login", (req, res) => {
-  res.send("User login successfully...");
+// app.post("/user/login", (req, res) => {
+//   res.send("User login successfully...");
+// });
+
+// app.get("/user/data", userAuth, (req, res) => {
+//   res.send("User data sent...");
+// });
+
+// app.get("/admin/getAllData", (req, res) => {
+//   res.send("Authorized user...");
+// });
+
+// app.get("/admin/deleteAllData", (req, res) => {
+//   res.send("deleted data...");
+// });
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong...");
+  }
 });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("User data sent...");
+app.get("/getUserData", (req, res) => {
+//   try {
+    throw new Error("efgkufq");
+    res.send("user data sent...");
+//   } catch (err) {
+//     res.status(500).send("Something cannot be detected...");
+//   }
 });
 
-app.get("/admin/getAllData", (req, res) => {
-  res.send("Authorized user...");
-});
-
-app.get("/admin/deleteAllData", (req, res) => {
-  res.send("deleted data...");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong...");
+  }
 });
 
 app.listen(7777, () => {
