3bef97a7108a487c92c7cd6abd8e36b8ddf65781
Anshika <anshikagoel1701@gmail.com>
2025-07-04 21:18:01 +0530

    index

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,11 +1,59 @@
 const express = require("express");
 const requestsRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const ConnectionRequest = require("../modules/connectionRequest");
+const User = require("../modules/user");
 
-requestsRouter.post("/sentConnectionRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  console.log("Connection is established");
-  res.send(user.firstName + " send the connection request...");
-});
+requestsRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const allowedStatus = ["ignored", "interested"];
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      const toUser = await User.findById(toUserId);
+      if (!toUser) {
+        return res.status(404).json({ message: "user does not exist" });
+      }
+
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection request already exists!" });
+      }
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Connection request sent successfully!",
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  }
+);
 
 module.exports = requestsRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,16 +1,36 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const { firstName, lastName, emailId, password } = req.body;
-    if(!firstName || !lastName){
-        throw new Error("Please enter a valid name...");
-    } else if(!validator.isEmail(emailId)){
-        throw new Error("Please enter valid email address...");
-    } else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter strong password...");
-    }
+  const { firstName, lastName, emailId, password } = req.body;
+  if (!firstName || !lastName) {
+    throw new Error("Please enter a valid name...");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Please enter valid email address...");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter strong password...");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedEditField = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "age",
+    "skills",
+    "about",
+    "photoURL",
+    "gender",
+  ];
+
+  const isAllowedEdit = Object.keys(req.body).every((field) =>
+    allowedEditField.includes(field)
+  );
+
+  req.send(isAllowedEdit);
 };
 
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData,
+  validateEditProfileData,
+};
