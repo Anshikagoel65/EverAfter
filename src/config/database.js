977f406abc05ef23e4d9abbaa02ab0bac41bf311
Anshika <anshikagoel1701@gmail.com>
2025-07-11 14:45:14 +0530

    render

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://anshikagoel1701:VDBZf8ESHSkHVq9r@namastenode.fdskjzc.mongodb.net/EverAfter"
-  );
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
 module.exports = connectDB;
